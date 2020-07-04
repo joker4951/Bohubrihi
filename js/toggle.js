7c82e21d4e8ac3933e5b2e0d3decfa1b419b7a0f
joker4951 <67384841+joker4951@users.noreply.github.com>
2020-07-05 02:32:19 +0600

    Add files via upload

---
+++ js/toggle.js
@@ -0,0 +1,9 @@
+function login() {
+	document.getElementById('loginform').style.display = "block";
+	document.getElementById('signupform').style.display = "none";
+}
+
+function signup() {
+	document.getElementById('loginform').style.display = "none";
+	document.getElementById('signupform').style.display = "block";
+}
